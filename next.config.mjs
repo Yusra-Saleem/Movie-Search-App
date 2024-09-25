7a9ce4f18de87141de0b362837309a18e74644e4
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-09-25 07:28:31 +0500

    Next.js Project

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [
-        'm.media-amazon.com',
-        'ia.media-imdb.com',
-        'image.tmdb.org',
-        'img.omdbapi.com',
-      ],
-    },
-    images: {
         remotePatterns: [
           {
             protocol: "https",
