cac24fa10d9f8ed6404a9271feddcd32e78f646e
Yusra-Saleem <rayyan726.m@gmail.com>
2024-09-25 06:46:29 +0500

    Next.js Project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'm.media-amazon.com',
+        'ia.media-imdb.com',
+        'image.tmdb.org',
+        'img.omdbapi.com',
+      ],
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "m.media-amazon.com",
+            port: "",
+          },
+        ],
+      },
+    };
+    
+   
+  
+  export default nextConfig;
+  
